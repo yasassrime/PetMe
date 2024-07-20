eb535d4d263e1dae2c0f30f7ce7806f7afa3c479
Yasas Sri Wickramasinghe <ureshan2011@gmail.com>
2024-07-20 22:05:14 +1200

    updates to cat adapt website

--- index.js
+++ index.js
@@ -62,10 +62,9 @@ function googleTranslateElementInit() {
 
 var imageIndex = 0;
 var imagesArray = [
-  "url('./Assets/Images/main-heading-body-bg.jpg') center",
-  "url('./Assets/Images/main-heading-body-bg2.png') center",
-  "url('./Assets/Images/main-heading-body-bg3.png') center",
-  "url('./Assets/Images/main-heading-body-bg4.png') center",
+  "url('./Assets/Images/catheader1.jpg') center",
+  "url('./Assets/Images/catheader2.jpg') center",
+  "url('./Assets/Images/catheader3.jpg') center",
 ];
 
 function changeBackground() {
